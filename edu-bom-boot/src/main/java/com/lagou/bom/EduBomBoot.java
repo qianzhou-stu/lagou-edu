4fa7c748a3ae2cd230874ef4594676efcdada86a
qian.zhou <1292184670@qq.com>
2022-06-12 21:52:04 +0800

    添加和修改配置文件信息、引入依赖信息和启动类

---
+++ edu-bom-boot/src/main/java/com/lagou/bom/EduBomBoot.java
@@ -0,0 +1,15 @@
+package com.lagou.bom;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@Slf4j
+@EnableDiscoveryClient
+public class EduBomBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(EduBomBoot.class, args);
+    }
+}
