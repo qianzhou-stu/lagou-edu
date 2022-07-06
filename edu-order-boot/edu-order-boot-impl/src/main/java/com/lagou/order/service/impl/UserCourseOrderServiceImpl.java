f5d479e6855809dce725d459e5c20baef81420c7
zhouqian19 <1!andreaszhou>
2022-07-06 18:20:36 +0800

    修改保存订单模块

--- edu-order-boot/edu-order-boot-impl/src/main/java/com/lagou/order/service/impl/UserCourseOrderServiceImpl.java
+++ edu-order-boot/edu-order-boot-impl/src/main/java/com/lagou/order/service/impl/UserCourseOrderServiceImpl.java
@@ -1,27 +1,38 @@
 package com.lagou.order.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.dangdang.ddframe.rdb.sharding.keygen.KeyGenerator;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lagou.common.constant.CacheDefine;
 import com.lagou.common.exception.ServiceException;
 import com.lagou.common.exception.SystemErrorType;
 import com.lagou.common.response.ResponseDTO;
 import com.lagou.common.util.ConvertUtil;
+import com.lagou.common.util.ValidateUtils;
 import com.lagou.course.api.ActivityCourseRemoteService;
 import com.lagou.course.api.CourseRemoteService;
 import com.lagou.course.api.dto.ActivityCourseDTO;
+import com.lagou.course.api.dto.CourseDTO;
+import com.lagou.course.api.enums.CourseStatus;
 import com.lagou.order.api.dto.CreateShopGoodsOrderReqDTO;
 import com.lagou.order.api.dto.UserCourseOrderDTO;
 import com.lagou.order.api.dto.UserCourseOrderResDTO;
 import com.lagou.order.api.enums.UserCourseOrderSourceType;
+import com.lagou.order.api.enums.UserCourseOrderStatus;
 import com.lagou.order.entity.UserCourseOrder;
 import com.lagou.order.mapper.UserCourseOrderMapper;
 import com.lagou.order.service.UserCourseOrderService;
 import javafx.beans.binding.ObjectExpression;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -33,11 +44,19 @@ import java.util.Objects;
  * @Version 1.0
  */
 @Service
+@Slf4j
 public class UserCourseOrderServiceImpl extends ServiceImpl<UserCourseOrderMapper, UserCourseOrder> implements UserCourseOrderService {
     @Autowired
     private UserCourseOrderMapper userCourseOrderMapper;
     @Autowired
     private ActivityCourseRemoteService activityCourseRemoteService;
+    @Autowired
+    private CourseRemoteService courseRemoteService;
+    @Autowired
+    private KeyGenerator keyGenerator;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
 
     @Override
     public Integer countUserCourseOrderByCourseIds(Integer userId, List<Integer> courseIds) {
@@ -50,6 +69,12 @@ public class UserCourseOrderServiceImpl extends ServiceImpl<UserCourseOrderMappe
         return userCourseOrderMapper.selectCount(queryWrapper);
     }
 
+
+    /**
+     * 创建订单
+     * @param reqDTO
+     * @return
+     */
     @Override
     public UserCourseOrderResDTO saveOrder(CreateShopGoodsOrderReqDTO reqDTO) {
         if (Objects.isNull(reqDTO.getGoodsId())){
@@ -61,10 +86,33 @@ public class UserCourseOrderServiceImpl extends ServiceImpl<UserCourseOrderMappe
         if (Objects.isNull(reqDTO.getSourceType())){
             return null;
         }
+        // 校验商品信息
+        CourseDTO courseDTO = courseRemoteService.getCourseById(reqDTO.getGoodsId(), reqDTO.getUserId());
+        log.info("saveOrder - courseRemoteService.getCourseById - goodsId:{} courseDTO:{}",reqDTO.getGoodsId(), JSON.toJSONString(courseDTO));
+        ValidateUtils.isFalse(null == courseDTO, "课程信息为空");
+        ValidateUtils.isTrue(courseDTO.getStatus().equals(CourseStatus.PUTAWAY.getCode()), "课程状态错误");
+        UserCourseOrder userCourseOrder = checkSuccessBuyGoods(reqDTO.getGoodsId(), reqDTO.getUserId());
+        ValidateUtils.isTrue(null == userCourseOrder, "已成功购买过该课程");
+        userCourseOrder = checkCreateBuyGoods(reqDTO.getGoodsId(), reqDTO.getUserId());
+        if(null != userCourseOrder) {
+            //已经下单还有效
+            return new UserCourseOrderResDTO(userCourseOrder.getOrderNo());
+        }
+        // 创建商品订单
+        UserCourseOrder saveOrder = buildUserCourseOrder(reqDTO.getGoodsId(), reqDTO.getUserId(), reqDTO.getSourceType());
+        String activityCourseStr = redisTemplate.opsForValue().get(CacheDefine.ActivityCourse.getKey(reqDTO.getGoodsId()));
+        ActivityCourseDTO activityCourseCache = null;
+        if (StringUtils.isNotBlank(activityCourseStr)){
+            activityCourseCache = JSON.parseObject(activityCourseStr, ActivityCourseDTO.class);
+            long cacheRes = redisTemplate.opsForValue().increment(CacheDefine.ActivityCourse.getStockKey(activityCourseCache.getCourseId()))
+        }
+        redisTemplate.opsForValue().increment();
+
+
         // 先根据商品id查询是不是为对应的活动课程
         ResponseDTO<ActivityCourseDTO> activityCourseDTOResponseDTO = activityCourseRemoteService.getByCourseId(reqDTO.getGoodsId());
         ActivityCourseDTO activityCourseDTO = activityCourseDTOResponseDTO.getContent();
-        UserCourseOrder userCourseOrder = new UserCourseOrder();
+        // UserCourseOrder userCourseOrder = new UserCourseOrder();
         // 生成订单号  -- 对于订单来说需要使用的是分库分表的方式使用
         userCourseOrder.setCourseId(reqDTO.getGoodsId());
         userCourseOrder.setUserId(reqDTO.getUserId());
@@ -72,6 +120,54 @@ public class UserCourseOrderServiceImpl extends ServiceImpl<UserCourseOrderMappe
 
 
         return null;
+    }
+
+    /**
+     * 查询用户成功状态订单
+     * @param goodId
+     * @param userId
+     * @return
+     */
+    UserCourseOrder checkSuccessBuyGoods(Integer goodId, Integer userId){
+        ValidateUtils.notNullParam(userId);
+        ValidateUtils.isTrue(userId > 0, "用户id错误");
+        ValidateUtils.notNullParam(goodId);
+        ValidateUtils.isTrue(goodId > 0, "课程id错误");
+        return getOne(new QueryWrapper<UserCourseOrder>().eq("course_id", goodId).eq("user_id", userId).eq("status", UserCourseOrderStatus.SUCCESS.getCode()));
+    }
+
+    /**
+     * 查询用户成功创建的订单状态
+     * @param goodId
+     * @param userId
+     * @return
+     */
+    UserCourseOrder checkCreateBuyGoods(Integer goodId, Integer userId){
+        ValidateUtils.notNullParam(userId);
+        ValidateUtils.isTrue(userId > 0, "用户id错误");
+        ValidateUtils.notNullParam(goodId);
+        ValidateUtils.isTrue(goodId > 0, "课程id错误");
+        return getOne(new QueryWrapper<UserCourseOrder>().eq("course_id", goodId).eq("user_id", userId).eq("status", UserCourseOrderStatus.CREATE.getCode()));
+    }
+
+    /**
+     * 构建商品订单信息
+     * @param goodId
+     * @param userId
+     * @param sourceType
+     * @return
+     */
+    UserCourseOrder buildUserCourseOrder(Integer goodId, Integer userId, UserCourseOrderSourceType sourceType){
+        UserCourseOrder saveUserCourseOrder = new UserCourseOrder();
+        // 使用sharding-jdbc的自动生成id的策略，自动生成对应的分库分表的id值
+        saveUserCourseOrder.setId(Long.parseLong(keyGenerator.generateKey().toString()));
+        saveUserCourseOrder.setCourseId(goodId);
+        saveUserCourseOrder.setCreateTime(new Date());
+        saveUserCourseOrder.setOrderNo(keyGenerator.generateKey().toString());
+        saveUserCourseOrder.setSourceType(sourceType.getCode());
+        saveUserCourseOrder.setUpdateTime(saveUserCourseOrder.getCreateTime());
+        saveUserCourseOrder.setUserId(userId);
+        return saveUserCourseOrder;
     }
 
     @Override
